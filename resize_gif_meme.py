1f37b787f46cae02d5ed401c9d130511ac407689
Tanat <ttanat@protonmail.com>
2020-12-11 23:33:37 +0000

    Convert to async function

--- resize_gif_meme.py
+++ resize_gif_meme.py
@@ -1,96 +1,74 @@
-import boto3, ffmpeg, os
-from secrets import token_urlsafe
-from operator import itemgetter
+import os
+
+import boto3
+import ffmpeg
+from PIL import Image
+
 
 s3 = boto3.client('s3')
-BUCKET = 'meme-page-london'
+BUCKET = "meme-page-london"
+
 
 def lambda_handler(event, context):
-    video_data = {
-        "folder": "large",
-        "filename": f"{token_urlsafe(5)}.mp4"
-    }
+    # Get image extension
+    ext = os.path.splitext(event["get_file_at"])[1]
 
-    thumbnail_data = {
-        "folder": "thumbnail",
-        "filename": f"{token_urlsafe(5)}.webp"
-    }
+    tmp_original_path = f"/tmp/original{ext}"
 
-    # Name of file in tmp directory
-    tmp = f"/tmp/{os.path.split(event['original_key'])[1]}"
+    # Download original image to original.<ext>
+    s3.download_file(BUCKET, event["get_file_at"], tmp_original_path)
 
-    # Download file from S3
-    s3.download_file(BUCKET, event["original_key"], tmp)
+    """ Create WEBP thumbnail """
+    # Open GIF
+    with Image.open(tmp_original_path).convert("RGB") as gif:
+        tmp_thumbnail_path = "/tmp/thumb.webp"
+        # Get image width and height
+        width, height = gif.size
+        # Get length of square to crop out of image
+        crop_size = min(gif.size)
+        # Crop and save thumbnail to tmp directory
+        with gif.crop((
+            (width - crop_size) // 2,
+            (height - crop_size) // 2,
+            (width + crop_size) // 2,
+            (height + crop_size) // 2
+        )) as tmp:
+            tmp.thumbnail((400, 400))
+            tmp.save(tmp_thumbnail_path, optimize=True, quality=70, format="WEBP")
+        # Upload WEBP thumbnail to S3
+        s3.upload_file(
+            tmp_thumbnail_path,
+            BUCKET,
+            event["thumbnail_key"],
+            ExtraArgs={"ContentType": "image/webp"}
+        )
 
-    # Get dimensions of gif
-    info = ffmpeg.probe(tmp)
-    width, height = itemgetter("width", "height")(info["streams"][0])
-
-    # Perform checks
-    if width < 280 or height < 280:
-        return {"statusCode": 418, "errorMessage": "GIF must be at least 280x280 pixels"}
-    if int(info["format"]["size"]) > 5242880:
-        return {"statusCode": 418, "errorMessage": "Maximum GIF size is 5MB"}
-
-    # Open file in ffmpeg
-    file = ffmpeg.input(tmp)
-
-    # Name of new video file in tmp directory
-    new_tmp = f"/tmp/video.mp4"
+    """ Create large mp4 file """
+    tmp_large_path = "/tmp/large.mp4"
+    # Remove file in tmp directory if exists (to avoid ffmpeg asking user input to overwrite file)
     try:
-        os.remove(new_tmp)
+        os.remove(tmp_large_path)
     except OSError:
         pass
-
-    # Process gif to video
+    # Open original GIF file
+    file = ffmpeg.input(tmp_original_path)
+    # Convert GIF to mp4 and save to "large.mp4" in tmp directory
     file.output(
-        new_tmp,
+        tmp_large_path,
         movflags="faststart",
         video_bitrate=0,
         crf=28,
         format="mp4",
         vcodec="libx264",
         pix_fmt="yuv420p",
-        vf="scale=trunc(iw/2)*2:trunc(ih/2)*2"
+        vf="scale='min(720,iw)':'min(720,ih)':force_original_aspect_ratio=decrease:force_divisible_by=2"
     ).run()
-
     # Upload video back to S3
     s3.upload_file(
-        new_tmp,
+        tmp_large_path,
         BUCKET,
-        os.path.join(event["path"], video_data["folder"], video_data["filename"]),
+        event["large_key"],
         ExtraArgs={"ContentType": "video/mp4"}
     )
 
-    # Create 400x400 WEBP thumbnail
-    # Name of thumbnail file in tmp directory
-    new_tmp = f"/tmp/thumbnail.webp"
-    try:
-        os.remove(new_tmp)
-    except OSError:
-        pass
-
-    # Create thumbnail
-    file.output(
-        new_tmp,
-        r=1,
-        vframes=1,
-        vf=f"scale='if(gt(iw,ih), -2, min(400,iw))':'if(gt(ih,iw), -2, min(400,ih))',\
-             crop='min(400,min(iw,ih))':'min(400,min(iw,ih))'"
-    ).run()
-
-    # Upload thumbnail back to S3
-    s3.upload_file(
-        new_tmp,
-        BUCKET,
-        os.path.join(event["path"], thumbnail_data["folder"], thumbnail_data["filename"]),
-        ExtraArgs={"ContentType": "image/webp"}
-    )
-
-    return {
-        "statusCode": 200,
-        "body": [
-            {"size": video_data["folder"], "filename": video_data["filename"]},
-            {"size": thumbnail_data["folder"], "filename": thumbnail_data["filename"]}
-        ]
-    }
+    return {"statusCode": 200}
